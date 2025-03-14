5c9014fd0a3999806cd9ef26cd8126f5fd99b33b
thai17asf <thai923717@gmail.com>
2025-03-14 15:14:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+require('dotenv').config();
+const { Client, GatewayIntentBits, ActivityType, PresenceUpdateStatus } = require('discord.js');
 
 const TOKEN = process.env.TOKEN;
-const CUSTOM_TEXT = "e-z.bio/thaixxp";
+const CUSTOM_TEXT = 'e-z.bio/thaixxp';
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-client.once("ready", () => {
+client.once('ready', () => {
     console.log(`✅ Bot ${client.user.tag} đã online!`);
 
     function updateStatus() {
@@ -17,13 +17,14 @@ client.once("ready", () => {
                 name: CUSTOM_TEXT,
                 type: ActivityType.Watching // Hoặc ActivityType.Playing tùy theo ý muốn
             }],
-            status: "dnd" // Trạng thái "Không làm phiền" với chấm đỏ
-        });
-        console.log("✅ Đã cập nhật trạng thái thành 'Không làm phiền' với thông điệp 'Đang bận'!");
+            status: PresenceUpdateStatus.DoNotDisturb // Trạng thái "Không làm phiền" với chấm đỏ
+        }).then(() => {
+            console.log('✅ Đã cập nhật trạng thái!');
+        }).catch(console.error);
     }
 
     updateStatus();
-    setInterval(updateStatus, 5000); // Cập nhật mỗi 30 giây
+    setInterval(updateStatus, 4000); // Cập nhật mỗi 4 giây
 });
 
 client.login(TOKEN);
